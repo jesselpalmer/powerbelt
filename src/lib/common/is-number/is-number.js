2411c9e537aab0c01915c3a88f49133cda6b2277
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-19 00:01:22 -0800

    add comments

--- src/lib/common/is-number/is-number.js
+++ src/lib/common/is-number/is-number.js
@@ -1,3 +1,10 @@
+/**
+ * isNumber()
+ * ----------
+ * Method that tests whether a value is a number or not.
+ * @param {any} value - The value that will be tested.
+ * @return {boolean} - Whether a value is a number or not.
+ */
 export default function isNumber(value) {
 	return typeof value === 'number' && arguments.length === 1;
 }
