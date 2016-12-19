e84b274f142a2edc3973b9d88f218d0f78920d88
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-18 19:34:16 -0800

    adds isBoolean tests

---
+++ src/lib/common/is-boolean/is-boolean.spec.js
@@ -0,0 +1,17 @@
+import isBoolean from './is-boolean';
+
+const expect = require('chai').expect;
+
+describe('common :: isBoolean tests', () => {
+	it('should be true for boolean', () => {
+		expect(isBoolean(true)).to.equal(true);
+	});
+
+	it('should be false for numbers', () => {
+		expect(isBoolean(4)).to.equal(false);
+	});
+
+	it('should be false for strings', () => {
+		expect(isBoolean("test")).to.equal(false);
+	});
+});
