94bd744cc044997d323f7483de92435e716ded68
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-18 20:20:43 -0800

    adds tests

--- src/lib/common/is-boolean/is-boolean.spec.js
+++ src/lib/common/is-boolean/is-boolean.spec.js
@@ -7,11 +7,37 @@ describe('common :: isBoolean tests', () => {
 		expect(isBoolean(true)).to.equal(true);
 	});
 
+	it('should be false for multiple booleans', () => {
+		expect(isBoolean(true, false)).to.equal(false);
+	});
+
 	it('should be false for numbers', () => {
 		expect(isBoolean(4)).to.equal(false);
 	});
 
 	it('should be false for strings', () => {
 		expect(isBoolean('test')).to.equal(false);
+	});
+
+	it('should be false for objects', () => {
+		expect(isBoolean({'name': 'gracie'})).to.equal(false);
+	});
+
+	it('should be false for arrays', () => {
+		expect(isBoolean([5, 1])).to.equal(false);
+	});
+
+	it('should be false for multiple numbers', () => {
+		expect(isBoolean(2, 3)).to.equal(false);
+	});
+
+	it('should be false for undefined values', () => {
+		let animal;
+		expect(isBoolean(animal)).to.equal(false);
+	});
+
+	it('should be false for null values', () => {
+		let animal = null;
+		expect(isBoolean(animal)).to.equal(false);
 	});
 });
