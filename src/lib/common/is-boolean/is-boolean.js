2d7a65166d16c05233bfcee44765d2441a73ae22
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-21 11:48:45 -0800

    add boolean comment

--- src/lib/common/is-boolean/is-boolean.js
+++ src/lib/common/is-boolean/is-boolean.js
@@ -1,3 +1,10 @@
-export default function (value) {
+/**
+ * isBoolean()
+ * -----------
+ * Method that tests whether a value is a boolean or not.
+ * @param {any} value - The value that will be tested.
+ * @return {boolean} - Whether a value is a boolean or not.
+ */
+export default function isBoolean (value) {
 	return typeof value === 'boolean' && arguments.length === 1;
 }
