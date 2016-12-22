a4af893268ecc8ef6a8a036f95419fffb188a67e
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-22 07:33:05 -0800

    comment added

--- src/lib/common/is-string/is-string.js
+++ src/lib/common/is-string/is-string.js
@@ -1,3 +1,10 @@
+/**
+ * isString()
+ * ----------
+ * Method that tests whether a value is a string or not.
+ * @param {any} value - The value that will be tested.
+ * @return {boolean} - Whether a value is a string or not.
+ */
 export default function isString(value) {
 	return typeof value === 'string' && arguments.length === 1;
 }
