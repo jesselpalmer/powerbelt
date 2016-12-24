405d1222dba47359ba0f0d01566fc518db7120e5
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-24 16:34:53 -0500

    add comments

--- src/lib/math/cube/cube.js
+++ src/lib/math/cube/cube.js
@@ -1,3 +1,10 @@
+/**
+ * Cube()
+ * ----------
+ * Method that returns a cubed number.
+ * @param {number} num - The number that will be cubed.
+ * @return {number} - Cubed number.
+ */
 export default function cube(num) {
 	return num * num * num;
 }
