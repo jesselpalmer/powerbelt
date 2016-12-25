54c94c157a0d4e0caaf7af5610c58e1c1173c6ea
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-25 15:56:16 -0500

    Update square.js

--- src/lib/math/square/square.js
+++ src/lib/math/square/square.js
@@ -1,3 +1,10 @@
+/**
+ * square()
+ * ----------
+ * Method that returns a squared number.
+ * @param {number} num - The number that will be squared.
+ * @return {number} - Squared number.
+ */
 export default function square(num) {
 	return num * num;
 }
