94c04cd213078b7e962daf2552d0d4f610cda738
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-27 14:33:38 -0500

    add comment

--- src/lib/math/is-even/is-even.js
+++ src/lib/math/is-even/is-even.js
@@ -1,3 +1,10 @@
+/**
+ * isEven()
+ * ----------
+ * Method that tests whether a number is an even.
+ * @param {any} num - The number that will be tested.
+ * @return {boolean} - Whether a number is a number or not.
+ */
 export default function isEven(num) {
 	return num % 2 === 0 && arguments.length === 1;
 }
