4ba2286107f8465bac300379c4e1afe61ef85b16
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-28 11:45:26 -0500

    add commits

--- src/lib/math/is-even/is-even.js
+++ src/lib/math/is-even/is-even.js
@@ -3,7 +3,7 @@
  * ----------
  * Method that tests whether a number is an even.
  * @param {any} num - The number that will be tested.
- * @return {boolean} - Whether a number is a number or not.
+ * @return {boolean} - Whether a number is even or not.
  */
 export default function isEven(num) {
 	return num % 2 === 0 && arguments.length === 1;
--- src/lib/math/is-odd/is-odd.js
+++ src/lib/math/is-odd/is-odd.js
@@ -1,3 +1,10 @@
+/**
+ * isOdd()
+ * ----------
+ * Method that tests whether a number is an odd.
+ * @param {any} num - The number that will be tested.
+ * @return {boolean} - Whether a number is odd or not.
+ */
 export default function isOdd(num) {
 	return num % 2 !== 0 && arguments.length === 1;
 }
