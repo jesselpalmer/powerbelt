d3a05706eb08822860dc2ebd4cbba7154e023f14
Jesse Palmer <jesselpalmer@gmail.com>
2016-12-29 12:05:50 -0800

    add comment

--- src/lib/math/is-squared/is-squared.js
+++ src/lib/math/is-squared/is-squared.js
@@ -1,3 +1,10 @@
+/**
+ * isSquared()
+ * ----------
+ * Method that returns whether or not a number is squared.
+ * @param {number} num - The number that will be checked to see if it is squared.
+ * @return {boolean} - The result of the squared check.
+ */
 export default function isSquared(num) {
 	let count = 0;
 
