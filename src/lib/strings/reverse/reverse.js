a66ad8adffa65012b6cd66191e16c2ee8924800e
Jesse Palmer <jesselpalmer@gmail.com>
2017-02-01 21:15:12 -0800

    refactored and added comments

--- src/lib/strings/reverse/reverse.js
+++ src/lib/strings/reverse/reverse.js
@@ -1,7 +1,15 @@
+/**
+ * reverse()
+ * ----------
+ * Method that returns a string in reverse.
+ * @param {word} string - The string that will be reversed.
+ * @return {string} - The reversed string.
+ */
 export default function reverse(word) {
   let reversedWord = '';
+  const startingPoint = word.length - 1;
 
-  for (let i = word.length - 1; i >= 0; i -= 1) {
+  for (let i = startingPoint; i >= 0; i -= 1) {
     reversedWord += word[i];
   }
 
