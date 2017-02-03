2aaedab7888690fa735fdd464675fcbe339b868f
Jesse Palmer <jesselpalmer@gmail.com>
2017-02-02 21:26:56 -0800

    refactor tests

--- src/lib/strings/reverse/reverse.spec.js
+++ src/lib/strings/reverse/reverse.spec.js
@@ -2,12 +2,12 @@ import reverse from './reverse';
 
 const expect = require('chai').expect;
 
-describe('reverse :: abs tests', () => {
-  it('should egabbaB selrahC', () => {
+describe('strings :: reverse tests', () => {
+  it('should be \'egabbaB selrahC\'', () => {
     expect(reverse('Charles Babbage')).to.equal('egabbaB selrahC');
   });
 
-  it('should be artskjiD .W regsdE', () => {
+  it('should be \'artskjiD .W regsdE\'', () => {
     expect(reverse('Edsger W. Dijkstra')).to.equal('artskjiD .W regsdE');
   });
 });
