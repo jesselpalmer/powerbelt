7f628b91b7b6207444d0bdf4081cee7b4d9ce8d6
Jesse Palmer <jesselpalmer@gmail.com>
2017-02-03 22:12:14 -0800

    finish init titleCase method

--- src/lib/strings/title-case/title-case.js
+++ src/lib/strings/title-case/title-case.js
@@ -1,3 +1,27 @@
 /**
- * Created by kzv874 on 1/30/17.
+ * titleCase()
+ * ----------
+ * Method that returns a string in Title Case.
+ * @param {str} string - The string that will be title cased.
+ * @return {string} - The title cased string.
  */
+export default function titleCase(str) {
+  const words = str.split(' ');
+  const len = words.length;
+  const result = [];
+  const ignoreWords = ['to', 'the', 'of'];
+
+  for (let i = 0; i < len; i += 1) {
+    let currentWord = words[i];
+
+    if (i === 0) {
+      currentWord = currentWord[0].toUpperCase() + currentWord.slice(1);
+    } else if (!ignoreWords.includes(currentWord)) {
+      currentWord = currentWord[0].toUpperCase() + currentWord.slice(1);
+    }
+
+    result.push(currentWord);
+  }
+
+  return result.join(' ');
+}
--- src/lib/strings/title-case/title-case.spec.js
+++ src/lib/strings/title-case/title-case.spec.js
@@ -1,3 +1,13 @@
-/**
- * Created by kzv874 on 1/30/17.
- */
+import titleCase from './title-case';
+
+const expect = require('chai').expect;
+
+describe('strings :: reverse tests', () => {
+  it('should be \'Introduction to Algorithms\'', () => {
+    expect(titleCase('introduction to algorithms')).to.equal('Introduction to Algorithms');
+  });
+
+  it('should be \'The Art of Computer Programming\'', () => {
+    expect(titleCase('the art of computer programming')).to.equal('The Art of Computer Programming');
+  });
+});
