b86a896c5a5bd13681c5a37662f66e091b0afe0c
Jesse Palmer <jesselpalmer@gmail.com>
2017-05-07 17:10:57 -0700

    remove sample files

--- src/lib/common/sample/sample.spec.js
+++
@@ -1,9 +0,0 @@
-import sample from './sample';
-
-const expect = require('chai').expect;
-
-describe('sample :: sample tests', () => {
-  it('should be 2', () => {
-    expect(sample(2)).to.equal(4);
-  });
-});
