d7fd686b21b9d6db5fda05827b2b6f87e25801df
Jesse Palmer <jesselpalmer@gmail.com>
2017-07-21 11:05:13 -0700

    refactor: supports checking multiple value

--- src/lib/common/is-boolean/is-boolean.js
+++ src/lib/common/is-boolean/is-boolean.js
@@ -2,9 +2,19 @@
  * isBoolean()
  * -----------
  * Method that tests whether a value is a boolean or not.
- * @param {any} value - The value that will be tested.
+ * @param {any} values - The values that will be tested.
  * @return {boolean} - Whether a value is a boolean or not.
  */
-const isBoolean = (...args) => typeof args[0] === 'boolean' && args.length === 1;
+const isBoolean = (...values) => { 
+  for (let i = 0; i < values.length; i++) {
+    const currentValue = values[i];
+
+    if (typeof currentValue !== 'boolean') {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 export default isBoolean;
--- src/lib/common/is-boolean/is-boolean.spec.js
+++ src/lib/common/is-boolean/is-boolean.spec.js
@@ -7,8 +7,23 @@ describe('common :: isBoolean tests', () => {
     expect(isBoolean(true)).to.be.equal(true);
   });
 
-  it('should be false for multiple booleans', () => {
-    expect(isBoolean(true, false)).to.be.equal(false);
+  it('should be true if multiple booleans are passed in', () => {
+    expect(isBoolean(true, false)).to.be.equal(true);
+    expect(isBoolean(true, true)).to.be.equal(true);
+    expect(isBoolean(false, true)).to.be.equal(true);
+    expect(isBoolean(false, false)).to.be.equal(true);
+    expect(isBoolean(false, false, true)).to.be.equal(true);
+    expect(isBoolean(false, false, false)).to.be.equal(true);
+    expect(isBoolean(false, false, false, true)).to.be.equal(true);
+  });
+
+  it('should be false if mulitple values are passed in and not all the values are a boolean', () => {
+    expect(isBoolean(true, 1)).to.be.equal(false);
+    expect(isBoolean(false, 'today', true)).to.be.equal(false);
+    expect(isBoolean('nice', 'today', true)).to.be.equal(false);
+    expect(isBoolean('nice', 1, true)).to.be.equal(false);
+    expect(isBoolean(true, 1, true)).to.be.equal(false);
+    expect(isBoolean(true, 1, true, { name: 'siliconvalley' })).to.be.equal(false);
   });
 
   it('should be false for numbers', () => {
@@ -25,10 +40,6 @@ describe('common :: isBoolean tests', () => {
 
   it('should be false for arrays', () => {
     expect(isBoolean([5, 1])).to.be.equal(false);
-  });
-
-  it('should be false for multiple numbers', () => {
-    expect(isBoolean(2, 3)).to.be.equal(false);
   });
 
   it('should be false for undefined values', () => {
