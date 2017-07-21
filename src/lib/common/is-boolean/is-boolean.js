8acff77466c86627615e908bb7aea29f7f599bcd
Jesse Palmer <jesselpalmer@gmail.com>
2017-07-20 22:58:17 -0700

    refactor: uses arrow function

--- src/lib/common/is-boolean/is-boolean.js
+++ src/lib/common/is-boolean/is-boolean.js
@@ -5,6 +5,6 @@
  * @param {any} value - The value that will be tested.
  * @return {boolean} - Whether a value is a boolean or not.
  */
-export default function isBoolean(value) {
-  return typeof value === 'boolean' && arguments.length === 1;
-}
+const isBoolean = (...args) => typeof args[0] === 'boolean' && args.length === 1;
+
+export default isBoolean;
