b4a673bc22b3b3f15515d7e37792417bb756c539
Jesse Palmer <jesselpalmer@gmail.com>
2017-07-22 13:40:43 -0700

    style: clean up

--- src/lib/common/is-boolean/is-boolean.js
+++ src/lib/common/is-boolean/is-boolean.js
@@ -5,6 +5,6 @@
  * @param {any} values - The values that will be tested.
  * @return {boolean} - Whether a value is a boolean or not.
  */
-const isBoolean = (...values) => values.every((value) => typeof value === 'boolean');
+const isBoolean = (...values) => values.every(value => typeof value === 'boolean')
 
-export default isBoolean;
+export default isBoolean
